4c690d5033a7417c34fc9827f360c86cd48e6ab1
Dragos Ille <illedragos@yahoo.com>
2026-05-22 20:20:11 +0300

    create a simple get request example and disaply port in logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3111);
+  const port = process.env.PORT ?? 3111;
+  await app.listen(port);
+  Logger.log(`Application running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  // Get /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return { currentLocation: location };
+  }
+
+  // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+}
